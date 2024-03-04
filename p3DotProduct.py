b1cbad9a998872868e901146e3459edbc60795fd
abeGizaw <gizawabe3@gmail.com>
2024-03-04 12:38:02 -0500

    coded a layer

---
+++ p3DotProduct.py
@@ -0,0 +1,27 @@
+#Ctr + B to run in Python
+
+# Values come from book
+inputs = [1, 2, 3, 2.5]
+
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+weights = [weights1, weights2, weights3]
+
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
+biases = [bias1, bias2, bias3]
+
+activations = []
+# Find the activation for all neurons
+for j in range(len(weights)):
+	activation = 0 
+	for k in range(len(inputs)):
+		activation += inputs[k] * weights[j][k]
+
+	activation += biases[j]
+	activations.append(activation)
+
+
+print(activations)
