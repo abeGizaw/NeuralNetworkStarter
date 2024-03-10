adfc127e57296e372de8f464d7cd371896db3ec5
abeGizaw <gizawabe3@gmail.com>
2024-03-09 19:24:57 -0500

    adding dot product logic

--- p1and2NeuronLayer.py
+++ p1and2NeuronLayer.py
@@ -6,12 +6,12 @@ inputs = [1, 2, 3, 2.5]
 weights1 = [0.2, 0.8, -0.5, 1.0]
 weights2 = [0.5, -0.91, 0.26, -0.5]
 weights3 = [-0.26, -0.27, 0.17, 0.87]
-weights = [weights1, weights2, weights3]
+weights = [weights1, weights2, weights3] #list of weights for each neuron. 
 
 bias1 = 2
 bias2 = 3
 bias3 = 0.5
-biases = [bias1, bias2, bias3]
+biases = [bias1, bias2, bias3] # Each Neurons biase's
 
 activations = []
 # Find the activation for all neurons
--- p3DotProduct.py
+++ p3DotProduct.py
@@ -6,21 +6,22 @@ inputs = [1, 2, 3, 2.5]
 weights1 = [0.2, 0.8, -0.5, 1.0]
 weights2 = [0.5, -0.91, 0.26, -0.5]
 weights3 = [-0.26, -0.27, 0.17, 0.87]
-weights = [weights1, weights2, weights3]
+weights = [weights1, weights2, weights3] #list of weights for each neuron. 
 
 bias1 = 2
 bias2 = 3
 bias3 = 0.5
-biases = [bias1, bias2, bias3]
+biases = [bias1, bias2, bias3] # Each Neuron's biases
 
 activations = []
 # Find the activation for all neurons
-for j in range(len(weights)):
+# zip(weights, biases) -> ((w1, b1), (w2,b2)..)
+for neuron_weights, neuron_bias in zip(weights, biases):
 	activation = 0 
-	for k in range(len(inputs)):
-		activation += inputs[k] * weights[j][k]
+	for neuron_input, weight in zip(inputs, neuron_weights):
+		activation += neuron_input * weight
 
-	activation += biases[j]
+	activation += neuron_bias
 	activations.append(activation)
 
 
