b54ff460c4be9c2e1e7151e234432a47bf6b376b
abeGizaw <gizawabe3@gmail.com>
2024-03-18 00:46:13 -0400

    adding training data and implementing forward func

--- p4ObjectsAndMatrixProducts.py
+++ p4ObjectsAndMatrixProducts.py
@@ -7,9 +7,25 @@ X = [[1, 2, 3, 2.5],
      [-1.5, 2.7, 3.3, -0.8]]
 
 class LayerDense:
-    def __init__(self):
-        print(np)
-        pass
-    def forward(self):
-        pass
+    def __init__(self, n_inputs, n_neurons):
+        self.output = None
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = LayerDense(4, 5)
+#has to have 5 as n_inputs since layer 1 has 5 neurons
+layer2 = LayerDense(5, 2)
+
+layer1.forward(X)
+print(layer1.output)
+print()
+layer2.forward(layer1.output)
+print(layer2.output)
+
+
+
+
  
